2055ab3dd4facd11895f6628336bfe3503f44fec
Giophx <ra2457088@MAC409.local>
2026-05-27 21:21:25 -0300

    final

--- components/EmojiList.tsx
+++ components/EmojiList.tsx
@@ -8,13 +8,15 @@ type Props = {
 };
 
 export default function EmojiList({ onSelect, onCloseModal }: Props) {
+
   const [emoji] = useState<ImageSourcePropType[]>([
-    require("../assets/images/emoji1.png"),
-    require("../assets/images/emoji2.png"),
-    require("../assets/images/emoji3.png"),
-    require("../assets/images/emoji4.png"),
-    require("../assets/images/emoji5.png"),
-    require("../assets/images/emoji6.png"),
+
+    require("../assets/images/65524.jpg"), 
+    require("../assets/images/7248699_3586955.jpg"),
+    require("../assets/images/16879826_tng6_dzm9_210603.jpg"),
+    require("../assets/images/20770793_qmgw_gd9h_210715.jpg"),
+    require("../assets/images/28428380_rm235batch7-noon-16a.jpg"),
+    require("../assets/images/34100612_rm223batch3-aew-42a.jpg"),
   ]);
 
   return (
