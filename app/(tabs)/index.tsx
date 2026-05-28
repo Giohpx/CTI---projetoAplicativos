a02749e10adfb6c5a66b94ca314ff25af411cdca
Giophx <ra2457088@MAC409.local>
2026-05-27 21:04:58 -0300

    novas atualizacoes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import { ImageSourcePropType, View, StyleSheet } from 'react-native';
+import { ImageSourcePropType, View, StyleSheet, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useState, useRef } from 'react';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
-
+import domtoimage from 'dom-to-image';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -17,8 +17,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-  
-  const imageRef = useRef (null);
+  const imageRef = useRef(null);
   const [status, requestPermission] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
@@ -55,32 +54,49 @@ export default function Index() {
     setIsModalVisible(false);
   };
 
-const onSaveImageAsync = async () => {
-  try {
-    const localUri = await captureRef(imageRef, {
-      height: 440,
-      quality: 1,
-    });
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-    await MediaLibrary.saveToLibraryAsync(localUri);
-    if (localUri) {
-      alert('Saved!');
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+     try {
+        if (imageRef.current) {
+          const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+            quality: 0.95,
+            width: 320,
+            height: 440,
+          });
+
+          let link = document.createElement('a');
+          link.download = 'sticker-smash.jpeg';
+          link.href = dataUrl;
+          link.click();
+        }
+      } catch (e) {
+        console.log(e);
+      }
     }
-  } catch (e) {
-    console.log(e);
-  }
-}; 
+  };
 
-return (
+  return (
     <GestureHandlerRootView style={styles.container}>
-
       <View style={styles.imageContainer}>
         <View ref={imageRef} collapsable={false}>
           <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
           {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
-        </View> { }
+        </View>
       </View>
-
 
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -96,12 +112,14 @@ return (
           <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
         </View>
       )}
+
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
     </GestureHandlerRootView>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,62 @@
-import { Stack, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { LogBox } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { StatusBar } from "expo-status-bar";
+
 LogBox.ignoreAllLogs(true);
 
-export default function TabLayout(){
-  return(
-
-
-     <Tabs
-      screenOptions={{
-      tabBarActiveTintColor: '#ff3d3dff',
-      }}>
-
-        <Tabs.Screen
-          name="index"
-          options={{
+export default function TabLayout() {
+  return (
+    <>
+      <StatusBar style="light" />
+      
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#ff3d3dff',
+          headerShown: false, 
+        }}
+      >
+        <Tabs.Screen 
+          name="index" 
+          options={{ 
             title: 'Home',
             tabBarIcon: ({ color, focused }) => (
               <MaterialCommunityIcons name={focused ? 'flower' : 'flower-outline'} color={color} size={24}/>
-
             ),
           }}
         />
         
-        <Tabs.Screen
+        <Tabs.Screen 
           name="about"
           options={{
             title: 'Sobre',
             tabBarIcon: ({ color, focused }) => (
               <MaterialCommunityIcons name={focused ? 'flower-pollen' : 'flower-pollen-outline'} color={color} size={24}/>
-
             ),
           }}
         />
+
         <Tabs.Screen
           name="toDoList"
           options={{
             title: 'To Do List',
-            tabBarIcon: ({ color, focused }) => (
-              <AntDesign name="calendar" size={24} color="black" />
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="calendar" size={24} color={color} />
             ),
           }}
         />
-         <Tabs.Screen
+
+        <Tabs.Screen
           name="seresDoMar"
           options={{
             title: 'Seres do Mar',
-            tabBarIcon: ({ color, focused }) => (
-              <AntDesign name="moon" size={24} color="black" />
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="moon" size={24} color={color} />
             ),
           }}
         />
-    </Tabs>
+      </Tabs>
+    </>
   );
-} 
+}
